78e00dadab82b630b6fab777c27239917d9bfb87
Octanoz <130052848+Octanoz@users.noreply.github.com>
2023-06-12 10:11:37 +0200

    Added invoice class

---
+++ AppointFirstTryWPF/Model/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointFirstTryWPF.Model
+{
+    public class Invoice
+    {
+        public int invoice_number { get; set; }
+        public string invoice_date { get; set; }
+        public decimal amount { get; set; }
+        public int client_id { get; set; }
+    }
+}
